a0c631cd85d1aa34d2911be87e4edf7116c62ef8
ibrahimtamimi <ibrahimtamimi5@gmail.com>
2017-04-08 17:54:10 +0300

    (feat) test

---
+++ server/test.js
@@ -0,0 +1,20 @@
+const app = require('./server.js')
+const request = require('supertest')
+const expect = require('chai').expect
+
+describe('Expeadia API ', function () {
+  it('Should get all hotels offers ', function (done) {
+    request(app)
+      .get('/api/data')
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .end(function (err, resp) {
+        if (err) {
+          throw new Error(err)
+        }
+        expect(resp.body).to.be.an('array')
+        done()
+      })
+  })
+})
